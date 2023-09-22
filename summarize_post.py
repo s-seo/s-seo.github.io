5075a3cc48905651afdf34e895113eec66218f3b
s-seo <baoro9394@gmail.com>
2023-09-22 21:43:42 +0900

    messeage and format

--- summarize_post.py
+++ summarize_post.py
@@ -11,20 +11,20 @@ def summarize_file(file_path):
     response = requests.post('http://43.201.66.120:8000/summarize', json = {'text': post_content})
     summarized_content = response.json()['summary']
 
-
+    front_matter, main_content = post_content.split('\n---')
+    
     with open(file_path, 'w') as f:
-        f.write(post_content.split('\n---')[0])
+        f.write(front_matter)
         f.write("\n---\n")
         f.write('TL;DR;')
         f.write('\n***\n')
         f.write(summarized_content)
         f.write('\n***\n')
-        f.write(post_content)
+        f.write(main_content)
 
     # with open(file_path, 'r') as f:
     #     print(f"Content of {file_path} after writing:")
     #     print(f.read())
-
 
 if __name__ == "__main__":
     changed_files = sys.argv[1].split()
@@ -33,7 +33,6 @@ if __name__ == "__main__":
         if file.endswith('.md') and 'docs/' in file:
             print(file)
             summarize_file(file)
-
 
 # response = requests.post('http://43.201.66.120:8000/summarize', json = {'text': post_content})
 # summarized_content = response.json()['summary']
