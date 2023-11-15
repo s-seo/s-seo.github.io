a8523e5df8c3faf8d2822ff5c44b8c425588ebc9
s-seo <baoro9394@gmail.com>
2023-11-15 15:13:21 +0900

    change summary api ip

--- summarize_post.py
+++ summarize_post.py
@@ -11,10 +11,10 @@ def summarize_file(file_path):
     length = 4000
     summarized_contents = []
     for i in range(0, len(post_content), length):
-        response = requests.post('http://43.201.66.120:8000/summarize', json = {'text': post_content[i:i + length]})
+        response = requests.post('http://13.124.73.243:8000/summarize', json = {'text': post_content[i:i + length]})
         summarized_contents.append(response.json()['summary'])
     
-    response = requests.post('http://43.201.66.120:8000/summarize', json = {'text': '\n'.join(summarized_contents)})
+    response = requests.post('http://13.124.73.243:8000/summarize', json = {'text': '\n'.join(summarized_contents)})
     summarized_content = response.json()['summary']
 
     front_matter, main_content = post_content.split('\n---')
@@ -33,13 +33,8 @@ def summarize_file(file_path):
         f.write('\n')
         f.write(main_content)
 
-    # with open(file_path, 'r') as f:
-    #     print(f"Content of {file_path} after writing:")
-    #     print(f.read())
-
 if __name__ == "__main__":
     changed_files = sys.argv[1].split(',')
-    # changed_files = ['docs/Study/CFReviewModel.md']
     print(changed_files)
     for file in changed_files:
         if file.endswith('.md') and 'docs/' in file:
@@ -47,8 +42,3 @@ if __name__ == "__main__":
             summarize_file(file)
         else:
             print('not end with .md')
-
-# response = requests.post('http://43.201.66.120:8000/summarize', json = {'text': post_content})
-# summarized_content = response.json()['summary']
-
-
